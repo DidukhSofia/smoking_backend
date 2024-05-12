f054b86ccec46a67a3796d79f6d8bcc568c38d37
DidukhSofia <diduhs0@gmail.com>
2024-05-12 09:57:47 +0300

    delete files

--- testing.py
+++
@@ -1,4 +0,0 @@
-from backend.app.models import User
-user = User.objects.create_user(email='user@example.com', password='password', startAmountOfCigarettes=10, priceOfPack=5, amountCigarettesInPack=20, progressDays=30)
-print(user.calculate_saved_cigarettes())
-print(user.calculate_saved_money())
